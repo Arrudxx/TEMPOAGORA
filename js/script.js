bf7c61f4fba8f29bdba68eae9d5812ea43a64be0
Arrudxx <d4niel.arruda@gmail.com>
2022-10-01 15:48:35 -0300

    declarada variaveis e seleção de elementos

--- js/script.js
+++ js/script.js
@@ -1,1 +1,11 @@
-//98aa04dae87ee9ff1135a71b64fc07f2
+// Variaveis e seleção de elementos
+
+const apiKey = "98aa04dae87ee9ff1135a71b64fc07f2";
+const apiCoutryURL = "https://countryflagsapi.com/png/";
+
+const Cidade = document.querySelector("#cidade-input");
+const btnPesquisa = document.querySelector("#btn-pesquisa");
+
+// funções
+
+// Eventos
